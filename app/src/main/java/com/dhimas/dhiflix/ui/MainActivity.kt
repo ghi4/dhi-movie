d0c535e3b097a081b4d0a8cd1a5b36beb77b013c
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-17 20:41:32 +0700

    Add movie poster and backdrop Add recycle view to fragment_home.xml Add item_movie.xml Add few values to strings.xml

--- app/src/main/java/com/dhimas/dhiflix/ui/MainActivity.kt
+++ app/src/main/java/com/dhimas/dhiflix/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.dhimas.dhiflix.ui
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -16,7 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
