b2eab1fc484b83c6deb736bd7c400677464aada7
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-17 18:48:54 +0700

    Add MovieEntity.kt and DummyData.kt for movies list

---
+++ app/src/main/java/com/dhimas/dhiflix/data/MovieEntity.kt
@@ -0,0 +1,10 @@
+package com.dhimas.dhiflix.data
+
+data class MovieEntity (
+    var title: String?,
+    var releaseYear: String?,
+    var score: String?,
+    var overview: String?,
+    var posterPath: String?,
+    var backdropPath: String?
+)
---
+++ app/src/main/java/com/dhimas/dhiflix/utils/DummyData.kt
@@ -0,0 +1,32 @@
+package com.dhimas.dhiflix.utils
+
+import com.dhimas.dhiflix.data.MovieEntity
+
+object DummyData {
+
+    fun generateDummyMovies(): ArrayList<MovieEntity>{
+        val movies = ArrayList<MovieEntity>()
+
+        movies.add(
+            MovieEntity(
+                "2067",
+                "2020",
+                "57",
+                "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
+                "path",
+                "path")
+        )
+
+        movies.add(
+            MovieEntity(
+                "Welcome to Sudden Death",
+                "2020",
+                "67",
+                "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
+                "path",
+                "path")
+        )
+
+        return movies
+    }
+}
