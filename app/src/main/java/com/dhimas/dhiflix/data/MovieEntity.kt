2d153c53cca3390b7958dfb29282da7f1195f0d6
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-18 09:28:03 +0700

    Make default value to null

--- app/src/main/java/com/dhimas/dhiflix/data/MovieEntity.kt
+++ app/src/main/java/com/dhimas/dhiflix/data/MovieEntity.kt
@@ -1,10 +1,10 @@
 package com.dhimas.dhiflix.data
 
 data class MovieEntity (
-    var title: String?,
-    var releaseYear: String?,
-    var score: String?,
-    var overview: String?,
-    var posterPath: String?,
-    var backdropPath: String?
+    var title: String? = null,
+    var releaseYear: String? = null,
+    var score: String? = null,
+    var overview: String? = null,
+    var posterPath: String? = null,
+    var backdropPath: String? = null
 )
