30061d281992ed0274c7d17c185f199555669d97
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-19 06:21:21 +0700

    Finish the series list

--- app/src/main/java/com/dhimas/dhiflix/utils/DummyData.kt
+++ app/src/main/java/com/dhimas/dhiflix/utils/DummyData.kt
@@ -12,7 +12,6 @@ object DummyData {
             MovieEntity(
                 "2067",
                 "2020",
-                "57",
                 "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
                 R.drawable.poster_2067,
                 R.drawable.backdrop_2067
@@ -23,7 +22,6 @@ object DummyData {
             MovieEntity(
                 "Welcome to Sudden Death",
                 "2020",
-                "67",
                 "Jesse Freeman is a former special forces officer and explosives expert now working a regular job as a security guard in a state-of-the-art basketball arena. Trouble erupts when a tech-savvy cadre of terrorists kidnap the team's owner and Jesse's daughter during opening night. Facing a ticking clock and impossible odds, it's up to Jesse to not only save them but also a full house of fans in this highly charged action thriller.",
                 R.drawable.poster_welcome_to_sudden_death,
                 R.drawable.backdrop_welcome_to_sudden_death
@@ -34,7 +32,6 @@ object DummyData {
             MovieEntity(
                 "Hard Kill ",
                 "2020",
-                "47",
                 "The work of billionaire tech CEO Donovan Chalmers is so valuable that he hires mercenaries to protect it, and a terrorist group kidnaps his daughter just to get it.",
                 R.drawable.poster_hardkill,
                 R.drawable.backdrop_hardkill
@@ -45,7 +42,6 @@ object DummyData {
             MovieEntity(
                 "Peninsula",
                 "2020",
-                "71",
                 "A soldier and his team battle hordes of post-apocalyptic zombies in the wastelands of the Korean Peninsula.",
                 R.drawable.poster_peninsula,
                 R.drawable.backdrop_peninsula
@@ -56,7 +52,6 @@ object DummyData {
             MovieEntity(
                 "Unknown Origins",
                 "2020",
-                "62",
                 "In Madrid, Spain, a mysterious serial killer ruthlessly murders his victims by recreating the first appearance of several comic book superheroes. Cosme, a veteran police inspector who is about to retire, works on the case along with the tormented inspector David Valentín and his own son Jorge Elías, a nerdy young man who owns a comic book store.",
                 R.drawable.poster_unknown_origins,
                 R.drawable.backdrop_unknown_origins
@@ -67,7 +62,6 @@ object DummyData {
             MovieEntity(
                 "Artemis Fowl",
                 "2020",
-                "58",
                 "Artemis Fowl is a 12-year-old genius and descendant of a long line of criminal masterminds. He soon finds himself in an epic battle against a race of powerful underground fairies who may be behind his father's disappearance.",
                 R.drawable.poster_artemis_fowl,
                 R.drawable.backdrop_artemis_fowl
@@ -78,7 +72,6 @@ object DummyData {
             MovieEntity(
                 "Hubie Halloween",
                 "2020",
-                "64",
                 "Hubie Dubois who, despite his devotion to his hometown of Salem, Massachusetts (and its legendary Halloween celebration), is a figure of mockery for kids and adults alike. But this year, something really is going bump in the night, and it’s up to Hubie to save Halloween.",
                 R.drawable.poster_hubie_halloween,
                 R.drawable.backdrop_hubie_halloween
@@ -89,7 +82,6 @@ object DummyData {
             MovieEntity(
                 "One Night in Bangkok",
                 "2020",
-                "72",
                 "A hit man named Kai flies into Bangkok, gets a gun, and orders a cab. He offers a professional female driver big money to be his all-night driver. But when she realizes Kai is committing brutal murders at each stop, it's too late to walk away. Meanwhile, an offbeat police detective races to decode the string of slayings before more blood is spilled.",
                 R.drawable.poster_one_night_in_bangkok,
                 R.drawable.backdrop_one_night_in_bangkok
@@ -100,7 +92,6 @@ object DummyData {
             MovieEntity(
                 "Battlefield 2025",
                 "2020",
-                "57",
                 "Weekend campers, an escaped convict, young lovers and a police officer experience a night of terror when a hostile visitor from another world descends on a small Arizona town.",
                 R.drawable.poster_battlefield_2025,
                 R.drawable.backdrop_battlefield_2025
@@ -111,54 +102,138 @@ object DummyData {
             MovieEntity(
                 "Sonic the Hedgehog",
                 "2020",
-                "75",
                 "Based on the global blockbuster videogame franchise from Sega, Sonic the Hedgehog tells the story of the world’s speediest hedgehog as he embraces his new home on Earth. In this live-action adventure comedy, Sonic and his new best friend team up to defend the planet from the evil genius Dr. Robotnik and his plans for world domination.",
                 R.drawable.poster_sonic_the_hegdog,
                 R.drawable.backdrop_sonic_the_hegdog
             )
         )
 
-//        movies.add(
-//            MovieEntity(
-//                "",
-//                "2020",
-//                "",
-//                "",
-//                ,
-//
-//            )
-//        )
-//
-//        movies.add(
-//            MovieEntity(
-//                "",
-//                "2020",
-//                "",
-//                "",
-//                ,
-//
-//            )
-//        )
-
         return movies
     }
 
     fun generateDummySeries(): ArrayList<MovieEntity>{
-        val movies = ArrayList<MovieEntity>()
+        val series = ArrayList<MovieEntity>()
 
-        movies.add(
-                MovieEntity(
-                        "Drakor",
-                        "2020",
-                        "57",
-                        "A lowly utility worker is called to the future by a mysterious radio signal, he must leave his dying wife to embark on a journey that will force him to face his deepest fears in an attempt to change the fabric of reality and save humankind from its greatest environmental crisis yet.",
-                        R.drawable.poster_peninsula,
-                        R.drawable.backdrop_hardkill
-                )
+        series.add(
+            MovieEntity(
+                "The Simpsons",
+                "1989",
+                "Set in Springfield, the average American town, the show focuses on the antics and everyday adventures of the Simpson family; Homer, Marge, Bart, Lisa and Maggie, as well as a virtual cast of thousands. Since the beginning, the series has been a pop culture icon, attracting hundreds of celebrities to guest star. The show has also made name for itself in its fearless satirical take on politics, media and American life in general.",
+                R.drawable.poster_the_simpson,
+                R.drawable.backdrop_the_simpson
+            )
         )
 
+        series.add(
+            MovieEntity(
+                "Game of Thrones",
+                "2011",
+                "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+                R.drawable.poster_got,
+                R.drawable.backdrop_got
+            )
+        )
 
+        series.add(
+            MovieEntity(
+                "The Good Doctor",
+                "2017",
+                "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives?",
+                R.drawable.poster_the_good_doctor,
+                R.drawable.backdrop_the_good_doctor
+            )
+        )
 
-        return movies
+        series.add(
+            MovieEntity(
+                "Naruto",
+                "2002",
+                "In another world, ninja are the ultimate power, and in the Village Hidden in the Leaves live the stealthiest ninja in the land. Twelve years earlier, the fearsome Nine-Tailed Fox terrorized the village and claimed many lives before it was subdued and its spirit sealed within the body of a baby boy. That boy, Naruto Uzumaki, has grown up to become a ninja-in-training who's more interested in pranks than in studying ninjutsu.. but Naruto is determined to become the greatest ninja ever!",
+                R.drawable.poster_naruto,
+                R.drawable.backdrop_naruto
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Vikings",
+                "2013",
+                "The adventures of Ragnar Lothbrok, the greatest hero of his age. The series tells the sagas of Ragnar's band of Viking brothers and his family, as he rises to become King of the Viking tribes. As well as being a fearless warrior, Ragnar embodies the Norse traditions of devotion to the gods. Legend has it that he was a direct descendant of Odin, the god of war and warriors.",
+                R.drawable.poster_vikings,
+                R.drawable.backdrop_vikings
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "The Mentalist",
+                "2008",
+                "Patrick Jane, a former celebrity psychic medium, uses his razor sharp skills of observation and expertise at \"reading\" people to solve serious crimes with the California Bureau of Investigation.",
+                R.drawable.poster_the_mentalist,
+                R.drawable.backdrop_the_mentalist
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Teen Wolf",
+                "2011",
+                "Scott McCall, a high school student living in the town of Beacon Hills has his life drastically changed when he's bitten by a werewolf, becoming one himself. He must henceforth learn to balance his problematic new identity with his day-to-day teenage life. The following characters are instrumental to his struggle: Stiles, his best friend; Allison, his love interest who comes from a family of werewolf hunters; and Derek, a mysterious werewolf with a dark past. Throughout the series, he strives to keep his loved ones safe while maintaining normal relationships with them.",
+                R.drawable.poster_teen_wolf,
+                R.drawable.backdrop_teen_wolf
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Arrow",
+                "2012",
+                "Spoiled billionaire playboy Oliver Queen is missing and presumed dead when his yacht is lost at sea. He returns five years later a changed man, determined to clean up the city as a hooded vigilante armed with a bow.",
+                R.drawable.poster_arrow,
+                R.drawable.backdrop_arrow
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Gotham",
+                "2014",
+                "Everyone knows the name Commissioner Gordon. He is one of the crime world's greatest foes, a man whose reputation is synonymous with law and order. But what is known of Gordon's story and his rise from rookie detective to Police Commissioner? What did it take to navigate the multiple layers of corruption that secretly ruled Gotham City, the spawning ground of the world's most iconic villains? And what circumstances created them – the larger-than-life personas who would become Catwoman, The Penguin, The Riddler, Two-Face and The Joker?",
+                R.drawable.poster_gotham,
+                R.drawable.backdrop_gotham
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Grimm",
+                "2011",
+                "After Portland homicide detective Nick Burkhardt discovers he's descended from an elite line of criminal profilers known as \"Grimms,\" he increasingly finds his responsibilities as a detective at odds with his new responsibilities as a Grimm.",
+                R.drawable.poster_grim,
+                R.drawable.backdrop_grim
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "The Mandalorian",
+                "2019",
+                "After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.",
+                R.drawable.poster_starwars_the_mandalorian,
+                R.drawable.backdrop_starwars_the_mandalorian
+            )
+        )
+
+        series.add(
+            MovieEntity(
+                "Prison Break",
+                "2005",
+                "Due to a political conspiracy, an innocent man is sent to death row and his only hope is his brother, who makes it his mission to deliberately get himself sent to the same prison in order to break the both of them out, from the inside out.",
+                R.drawable.poster_prison_break,
+                R.drawable.backdrop_prison_break
+            )
+        )
+
+        return series
     }
 }
