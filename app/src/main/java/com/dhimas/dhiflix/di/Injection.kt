72c1ce3e7d37f4b5877365a6d9726b2b3e2a410f
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-22 13:24:29 +0700

    Inject Retrofit

--- app/src/main/java/com/dhimas/dhiflix/di/Injection.kt
+++ app/src/main/java/com/dhimas/dhiflix/di/Injection.kt
@@ -2,7 +2,13 @@ package com.dhimas.dhiflix.di
 
 import com.dhimas.dhiflix.data.ShowRepository
 import com.dhimas.dhiflix.data.source.remote.RemoteDataSource
+import com.dhimas.dhiflix.data.source.remote.retrofit.RetrofitFactory
 
 object Injection {
+    fun provideRepository(): ShowRepository {
 
+        val remoteRepository = RemoteDataSource.getInstance(RetrofitFactory.makeRetrofitService())
+
+        return ShowRepository.getInstance(remoteRepository)
+    }
 }
