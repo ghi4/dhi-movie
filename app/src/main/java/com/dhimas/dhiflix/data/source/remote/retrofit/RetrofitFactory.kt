466f52a52c18d93a421b4b171acf4190743ea6d5
dhimas18 <ghidet4pcoop@gmail.com>
2020-10-24 15:08:49 +0700

    Final

--- app/src/main/java/com/dhimas/dhiflix/data/source/remote/retrofit/RetrofitFactory.kt
+++ app/src/main/java/com/dhimas/dhiflix/data/source/remote/retrofit/RetrofitFactory.kt
@@ -1,16 +1,12 @@
 package com.dhimas.dhiflix.data.source.remote.retrofit
 
-import dagger.Module
-import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-@Module
 class RetrofitFactory {
     companion object {
         private const val BASE_URL = "https://api.themoviedb.org/3/"
 
-        @Provides
         fun makeRetrofitService(): RetrofitInterface {
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_URL)
