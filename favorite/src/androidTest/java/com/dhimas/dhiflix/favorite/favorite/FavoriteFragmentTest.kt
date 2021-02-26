0fb2753a4b1173bbddd7baa225c386a71e1f881d
dhimas18 <ghidet4pcoop@gmail.com>
2021-02-26 22:03:57 +0700

    Add instrumen test

--- favorite/src/androidTest/java/com/dhimas/dhiflix/favorite/favorite/FavoriteFragmentTest.kt
+++ favorite/src/androidTest/java/com/dhimas/dhiflix/favorite/favorite/FavoriteFragmentTest.kt
@@ -1,14 +1,7 @@
 package com.dhimas.dhiflix.favorite.favorite
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.dhimas.dhiflix.favorite.R
 import com.dhimas.dhiflix.ui.main.MainActivity
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
